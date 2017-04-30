702eec04b017d72309e384c36ca3649a85f6488c
hellcat <hellcat@gatoatlas.(none)>
2017-04-30 23:17:06 +0200

    Updated to a correct setup.sh

--- release-downloader/setup.sh
+++ release-downloader/setup.sh
@@ -1,32 +1,5 @@
-#
-#-- Setup script made by Pacman 3.29 and is often regenerated.  DO NOT EDIT
-#
-#
-#-- begin pre-setup
-#
-export PAC_ANCHOR="/mnt/swia/tmp"
-if [ -d "${PAC_ANCHOR}/pre-setup" ]; then
-    for PACMANi in `/usr/bin/env find "${PAC_ANCHOR}/pre-setup" -maxdepth 1 -type f -name '*.sh' | sort`; do
-        source "${PACMANi}"
-    done
-    unset PACMANi
-fi
-#
-#-- end pre-setup
-#
-#
-#-- package /mnt/swia/tmp:http://atlas.web.cern.ch/Atlas/GROUPS/DATABASE/pacman4/DBRelease:DBRelease-current
-#
-#
-#-- begin post-setup
-#
-export PAC_ANCHOR="/mnt/swia/tmp"
-if [ -d "${PAC_ANCHOR}/post-setup" ]; then
-    for PACMANi in `/usr/bin/env find "${PAC_ANCHOR}/post-setup" -maxdepth 1 -type f -name '*.sh' | sort`; do
-        source "${PACMANi}"
-    done
-    unset PACMANi
-fi
-#
-#-- end post-setup
-#
+#!/bin/bash
+
+DIR=`readlink -f $(dirname $0)`
+
+perl -pi.bak -e "s!reposdir=.*!reposdir=${DIR}/etc/yum.repos.d!" yum.conf
