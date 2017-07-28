36c627ab2b9ce0c617d6b9a3de3b6506533ace35
Pavlo Svirin <pavlo.svirin@cern.ch>
2017-07-27 20:42:40 -0400

    Launcher installer received new options (server and module) and new hierarchy for destination dir

--- titan-scripts/launcher_installer.sh
+++ titan-scripts/launcher_installer.sh
@@ -2,8 +2,13 @@
 
 function help(){
 	echo "Pilot launcher installer for ORNL Titan"
-	echo "Usage: launcher_installer.sh <experiment> <base_directory> <wms_queue>"
+	echo "Usage: launcher_installer.sh <experiment> <base_directory> <wms_queue> [<experiment_module>] [<panda_server>]"
 }
+
+if [[ -z $* ]]; then
+	help
+	exit 1
+fi
 
 if [[ -z $1 ]]; then
 	echo No experiment specified
@@ -17,8 +22,17 @@ if [[ -z $2 ]]; then
 	exit 1
 fi
 
-if [[ -e $2 ]]; then
-	echo Base directory already exists
+if [[ ! -w $2 ]]; then
+	echo Base directory does not exist
+	exit 1
+fi
+
+BASE_DIR=$2/$1/titan_utils
+
+echo We will install the toolset into $BASE_DIR
+
+if [[ -e $BASE_DIR ]]; then
+	echo Destination directory already exists
 	exit 1
 fi
 
@@ -28,16 +42,29 @@ if [[ -z $3 ]]; then
 	exit 1
 fi
 
-mkdir -p $2/pilots_logs_v2_$1 $2/pilots_workdir_$1
-cp -r ~/dev/titan_utils/* $2
-cp -r $2/launcher/nedm $2/launcher/$1
-rm -rf $2/launcher/nedm $2/launcher/lsst
-mv $2/multijob_pilot_lsst_src $2/multijob_pilot_$1_src
+EXPERIMENT_MODULE=ATLAS
+if [[ ! -z $4 ]]; then
+	EXPERIMENT_MODULE=$4
+fi
 
-cat << EOF > $2/launcher/$1/core_launcher_settings.py 
+PANDA_SERVER=pandaserver.cern.ch
+if [[ ! -z $5 ]]; then
+	PANDA_SERVER=$5
+fi
+
+mkdir -p $BASE_DIR/pilots_logs_v2_$1 $BASE_DIR/pilots_workdir_$1
+#cp -r ~/dev/titan_utils/* $BASE_DIR
+cp -r ./titan_utils/* $BASE_DIR
+cp -r $BASE_DIR/launcher/nedm $BASE_DIR/launcher/$1
+rm -rf $BASE_DIR/launcher/nedm $BASE_DIR/launcher/lsst
+mv $BASE_DIR/multijob_pilot_lsst_src $BASE_DIR/multijob_pilot_$1_src
+
+cat << EOF > $BASE_DIR/launcher/$1/core_launcher_settings.py 
 experiment = "$1"
-base_dir = "$2"
+base_dir = "$BASE_DIR"
 wms_queue = "$3"
+experiment_module = "$EXPERIMENT_MODULE"
+panda_server = "$PANDA_SERVER"
 EOF
 
 cat << EOF 
@@ -50,7 +77,7 @@ export X509_CERT_DIR=\$HOME/grid-security2/certificates
 
 voms-proxy-init -voms lsst:/lsst/Role=pilot -valid 48:00 -vomslife 48:00
 
-export $1_LAUNCHER_DIR=$2
+export $1_LAUNCHER_DIR=$BASE_DIR
 
 cd $1_LAUNCHER_DIR
 EOF
